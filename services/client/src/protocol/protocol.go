cdf693304baf5af96f01cbe6f1c4e97ee4158a4e
AlePaff <alejandropaff@gmail.com>
2026-09-03 18:08:24 -0300

    protocolo falto commitear

--- services/client/src/protocol/protocol.go
+++ services/client/src/protocol/protocol.go
@@ -2,7 +2,10 @@ package protocol
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/safe_socket"
 )
@@ -65,6 +68,51 @@ type Bet struct {
 	Number    uint32
 }
 
-func SerializeBet(bet Bet) ([]byte, error)
+// recibe una apuesta y la serieliza en un slice de bytes
+// el payload va separado por comas
+func SerializeBet(bet Bet) ([]byte, error) {
+	fields := []string{
+		strconv.FormatUint(uint64(bet.AgencyID), 10), // 10 es la base decimal
+		bet.FirstName,
+		bet.LastName,
+		strconv.FormatUint(uint64(bet.Document), 10),
+		bet.Birthdate,
+		strconv.FormatUint(uint64(bet.Number), 10),
+	}
 
-func DeserializeBet(data []byte) (Bet, error)
+	return []byte(strings.Join(fields, ",")), nil
+}
+
+// recibe un slice de bytes y lo deserializa en una apuesta Bet
+func DeserializeBet(data []byte) (Bet, error) {
+	fields := strings.Split(string(data), ",")
+
+	// esperamos 6 campos: agency_id, first_name, last_name, document, birthdate, number
+	if len(fields) != 6 {
+		return Bet{}, fmt.Errorf("invalid bet: expected 6 fields, got %d", len(fields))
+	}
+
+	agencyID, err := strconv.ParseUint(fields[0], 10, 32)
+	if err != nil {
+		return Bet{}, fmt.Errorf("invalid agency id: %w", err)
+	}
+
+	document, err := strconv.ParseUint(fields[3], 10, 32)
+	if err != nil {
+		return Bet{}, fmt.Errorf("invalid document: %w", err)
+	}
+
+	number, err := strconv.ParseUint(fields[5], 10, 32)
+	if err != nil {
+		return Bet{}, fmt.Errorf("invalid number: %w", err)
+	}
+
+	return Bet{
+		AgencyID:  uint32(agencyID),
+		FirstName: fields[1],
+		LastName:  fields[2],
+		Document:  uint32(document),
+		Birthdate: fields[4],
+		Number:    uint32(number),
+	}, nil
+}
