4fcd74f290570c03bd4c5b16f940c2242572e99f
AlePaff <alejandropaff@gmail.com>
2026-09-08 13:09:19 -0300

    feat: implmentacion basica del quorum. mismo archivo

--- services/server/src/server/server.py
+++ services/server/src/server/server.py
@@ -1,19 +1,23 @@
 import os
 import socket
+import threading
 import logger
 from protocol.protocol import MESSAGE_TYPE_BET, MESSAGE_TYPE_END, MESSAGE_TYPE_WINNERS, MESSAGE_TYPE_ACK, deserialize_bets_batch, receive_message, send_message, serialize_winners
 from lottery.lottery import Lottery
 
 
 class Server:
-    def __init__(self, server_host: str, server_port: int) -> None:
+    def __init__(self, server_host: str, server_port: int, agency_quorum_min: int) -> None:
         self.server_host = server_host
         self.server_port = server_port
+        self.agency_quorum_min = agency_quorum_min
         # vaciar archivo bets.csv al iniciar el servidor. Si no existe crearlo
         os.makedirs("/data", exist_ok=True)
         with open("/data/bets.csv", "w") as f:
             f.write("")
         self.lottery = Lottery("/data/bets.csv")
+        self.finished_agencies = set()          # set para evitar contar dos veces a una agencia
+        self.quorum_condition = threading.Condition()
         
 
     def _handle_client(self, client_socket):
@@ -29,13 +33,23 @@ class Server:
             raise e
 
     def _receive_bets(self, client_socket):
+        agency_id = None
+
         while True:
             message_type, payload = receive_message(client_socket)
 
             if message_type == MESSAGE_TYPE_BET:
                 betsBatch = deserialize_bets_batch(payload)
 
+                if(betsBatch is None or len(betsBatch) == 0):
+                    logger.error("receive-bets", logger.LogResult.fail, "err", "Se recibio un lote de apuestas vacio, se continua")
+                    continue
+
                 print("Batch recibido:", betsBatch)
+
+                # guarda el agency_id
+                if agency_id is None:
+                    agency_id = betsBatch[0].agency_id
 
                 self.lottery.store_bets(betsBatch)
 
@@ -44,6 +58,14 @@ class Server:
 
             elif message_type == MESSAGE_TYPE_END:
                 print(f"El cliente terminó de enviar apuestas. Se guardan las apuestas")
+                with self.quorum_condition:         # toma el lock, al salir de aqui se libera
+                    self.finished_agencies.add(agency_id)       # es para que solo uno a la vez pueda agregarse en finished_agencies
+
+                    self.quorum_condition.notify_all()      # despierta a los threads que estan esperando el quorum
+
+                    # si aún no se llegó al quorum, pone a dormir al hilo actual
+                    while len(self.finished_agencies) < self.agency_quorum_min:         # solo saldra del ciclo dormir->ser despertado->comprobar condicion->dormir, cuando se cumpla la condición
+                        self.quorum_condition.wait()        #aqui tambien se libera el lock del "with"
                 break
 
     def _calculate_winners(self):
@@ -84,9 +106,15 @@ class Server:
                 try:
                     logger.info(action, logger.LogResult.in_progress)
                     client_socket, _ = server_socket.accept()
+
+                    thread = threading.Thread(
+                        target=self._handle_client,
+                        args=(client_socket,)
+                    )
+                    thread.start()
+                    
                 except Exception as e:
                     logger.error(action, logger.LogResult.fail)
                     raise e
                 logger.info(action, logger.LogResult.success)
 
-                self._handle_client(client_socket)
